72a0039374a37e5f8dc823c0d3a17c6fd1e07f6d
Azam <aizen7737@gmail.com>
2026-01-28 12:19:32 +0530

    fixed responsive issues

--- src/pages/playerPage.jsx
+++ src/pages/playerPage.jsx
@@ -23,20 +23,20 @@ const PlayerPage = () => {
             <div className="bg-yellow-500 -skew-x-12 py-3 space-y-3 w-[95%] mx-auto relative">
               <h1 className="text-4xl font-bold lg:text-6xl font-oswald text-center text-black uppercase">{player?.name}</h1>
             </div>
-            <p className="text-left text-xl text-yellow-500">Date of Birth : <span className="text-white bg-none">{player?.dateOfBirth}</span></p>
-            <p className="text-left text-xl text-yellow-500">Father's Name : <span className="text-white">{player?.fatherName}</span></p>
-            <p className="text-left text-xl text-yellow-500">Place of Birth : <span className="text-white">{player?.placeOfBirth}</span></p>
+            <p className="text-left lg:text-xl text-yellow-500">Date of Birth : <span className="text-white bg-none">{player?.dateOfBirth}</span></p>
+            <p className="text-left lg:text-xl text-yellow-500">Father's Name : <span className="text-white">{player?.fatherName}</span></p>
+            <p className="text-left lg:text-xl text-yellow-500">Place of Birth : <span className="text-white">{player?.placeOfBirth}</span></p>
             {player?.debuts?.map((debut, index) => (
-              <p className="text-left text-xl text-yellow-500" key={index}> {debut.title} : <span className="text-white">{debut.value}</span></p>
+              <p className="text-left lg:text-xl text-yellow-500" key={index}> {debut.title} : <span className="text-white">{debut.value}</span></p>
             ))}
-            <p className="text-left text-xl text-yellow-500">Playing Role : <span className="text-white">{player?.playingRole}</span></p>
-            <p className="text-left text-xl text-yellow-500">Batting Style : <span className="text-white">{player?.battingStyle}</span></p>
-            <p className="text-left text-xl text-yellow-500">Bowling Style : <span className="text-white">{player?.bowlingStyle}</span></p>
-            <p className="text-left text-xl text-yellow-500">Major Achievement : <span className="text-white">{player?.achievement}</span></p>
+            <p className="text-left lg:text-xl text-yellow-500">Playing Role : <span className="text-white">{player?.playingRole}</span></p>
+            <p className="text-left lg:text-xl text-yellow-500">Batting Style : <span className="text-white">{player?.battingStyle}</span></p>
+            <p className="text-left lg:text-xl text-yellow-500">Bowling Style : <span className="text-white">{player?.bowlingStyle}</span></p>
+            <p className="text-left lg:text-xl text-yellow-500">Major Achievement : <span className="text-white">{player?.achievement}</span></p>
             {player?.best?.map((best, index) => (
-              <p className="text-left text-xl text-yellow-500" key={index}> {best.title} : <span className="text-white">{best.value}</span></p>
+              <p className="text-left lg:text-xl text-yellow-500" key={index}> {best.title} : <span className="text-white">{best.value}</span></p>
             ))}
-            <p className="text-left text-xl text-yellow-500">Bio : <span className="text-white">{player?.bio}</span></p>
+            <p className="text-left lg:text-xl text-yellow-500">Bio : <span className="text-white">{player?.bio}</span></p>
           </div>
         </div>
 
